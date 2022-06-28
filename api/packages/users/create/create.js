4ccfb6af813078890cd0fdf4f864d4c84f454537
Andy Burke <andy.burke@mailbox.earth>
2022-06-28 00:11:47 -0700

    feature: store a user?

--- api/packages/users/create/create.js
+++ api/packages/users/create/create.js
@@ -1,13 +1,69 @@
-const simplecoder = require( 'simplecoder' );
+const {
+	as_json_schema,
+	databases,
+	datatypes,
+	model
+} = require( 'databaser' );
+
+const User = model( {
+	name: 'user',
+	schema: {
+		id: datatypes.UUID( {
+			nullable: false,
+			unique: true,
+			primary: true
+		} ),
+		email: datatypes.email( {
+			initial: null,
+			index: true // add a basic index for this column
+		} ),
+		name: {
+			first: datatypes.string( {
+				initial: null
+			} ),
+			last: datatypes.string( {
+				initial: null
+			} )
+		},
+		meta: datatypes.JSON(),
+		timestamps: {
+			created: datatypes.ISODate(),
+			updated: datatypes.ISODate(),
+			deleted: datatypes.ISODate( {
+				initial: null
+			} )
+		}
+	}
+} );
 
 module.exports = {
 	main: function( args ) {
-		const new_user = {
-			email: args.email
+		const new_user_info = {
+			email: args.email,
+			name: {
+				first: args.name__first,
+				last: args.name__last
+			},
+			meta: {}
 		};
 
+		const new_user = User.create( new_user_info );
+	
+		console.dir( new_user );
+	
+		const validation_errors = User.validate( new_user );
+		if ( validation_errors.length ) {
+			throw new Error( `Invalid user:
+			${ JSON.stringify( validation_errors, null, 4 ) }
+			` );
+		}
+	
+		const users_db = await databases.postgres.get( User );
+
+		await users_db.put( new_user );
+
 		return {
-			body: simplecoder.encode( new_user.email ?? 'test' )
+			body: new_user
 		};
 	}
 };
